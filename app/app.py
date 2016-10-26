5a5a8d20904a16750aaba7e015e5d062532ae796
c14509343 <c14509343@mydit.ie>
2016-10-26 14:33:23 +0000

    web app running'

---
+++ app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def hello():
+
+    return "Hello Flask from DIT DT228 Program!"
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s'% username
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=3000, debug=True)
+
